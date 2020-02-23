00f3f8fa1a3fdb5ed632a2e91ec057353a521ab3
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-23 15:30:41 -0700

    Test for shot_accuracy by team_id

--- test/league_stats_test.rb
+++ test/league_stats_test.rb
@@ -110,6 +110,10 @@ class LeagueStatsTest < Minitest::Test
     assert_equal 6, @league_stats.total_shots_by_team_id(1)
   end
 
+  def test_shot_accuracy_by_team_id
+    assert_equal 33, @league_stats.shot_accuracy_by_team_id(1)
+  end
+
   def test_total_goals_by_team_id
     assert_equal 2, @league_stats.total_goals_by_team_id(1)
   end
