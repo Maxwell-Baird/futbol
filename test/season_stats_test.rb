2a568fe6eaf2332de28ee510295d70e80ef2cce3
Alex Latham <alexlthm@gmail.com>
2020-02-23 16:30:40 -0700

    Add winningest_coach test to SeasonStatsTest

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -15,4 +15,8 @@ class SeasonStatsTest < Minitest::Test
     @stat_tracker = StatTracker.from_csv(@locations)
     @season_stats = SeasonStats.new(@stat_tracker.games, @stat_tracker.teams, @stat_tracker.game_teams)
   end
+
+  def test_winningest_coach
+    assert_equal "Bruce Boudreau", @season_stats.winningest_coach(20142015)
+  end
 end
