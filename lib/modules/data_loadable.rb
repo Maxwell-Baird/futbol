ffed663faf572aa5f5ca799dd56476228d18dd80
Alex Latham <alexlthm@gmail.com>
2020-02-23 07:51:43 -0700

    Replace requires with require_relatives when Stats superclass is linked in GameStats, LeagueStats, SeasonStats, and TeamStats. Fix number of arguments error on super for GameStats class.

--- lib/modules/data_loadable.rb
+++ lib/modules/data_loadable.rb
@@ -1,7 +1,6 @@
 require 'csv'
 
 module DataLoadable
-
   def csv_data(file_path, object)
     csv = CSV.open(file_path, headers: :first_row, header_converters: :symbol)
     csv.map{ |row| object.new(row) }
