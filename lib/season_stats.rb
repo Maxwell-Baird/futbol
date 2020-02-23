c371596a9ec282d89eaa45e8f30007ce644226c0
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-23 15:03:20 -0700

    Setup methods for most/least accurate teams

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -4,4 +4,12 @@ class SeasonStats < Stats
   def initialize(games, teams, game_teams)
     super(games, teams, game_teams)
   end
+
+  def most_accurate_team(season_id)
+    # Name of the Team with the best ratio of shots to goals for the season	String
+  end
+
+  def least_accurate_team(season_id)
+    # Name of the Team with the worst ratio of shots to goals for the season
+  end
 end
