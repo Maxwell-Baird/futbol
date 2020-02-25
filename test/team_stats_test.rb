74d8d6473a268812fea3e5f500f1523d94c04d76
Alex Latham <alexlthm@gmail.com>
2020-02-24 17:44:26 -0700

    Add test for biggest_team_blowout

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -47,6 +47,17 @@ class TeamStatsTest < Minitest::Test
   end
 
   def test_it_can_return_biggest_team_blowout
-    skip
+    game_path = './data/games_truncated_with_winningest_team.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams_truncated.csv'
+    locations = {
+                games: game_path,
+                teams: team_path,
+                game_teams: game_teams_path
+              }
+    stat_tracker = StatTracker.from_csv(locations)
+    team_stats = TeamStats.new(stat_tracker.games, stat_tracker.teams, stat_tracker.game_teams)
+
+    assert_equal "Seattle Sounders FC", team_stats.biggest_team_blowout(1)
   end
 end
