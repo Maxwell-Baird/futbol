12ec0f01144b3dc8c6b43a007725a66fa0e046fc
Maxwell Baird <bairdm71@gmail.com>
2020-02-25 08:42:39 -0700

    created the head to head method

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -4,4 +4,8 @@ class TeamStats < Stats
   def initialize(games, teams, game_teams)
     super(games, teams, game_teams)
   end
+
+  def head_to_head(team_id)
+    all_games = games.find_all{ |game| team_id == game.away_team_id || team_id == game.home_team_id}
+  end
 end
