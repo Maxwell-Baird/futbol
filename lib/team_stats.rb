3b722e7e9b13ee2ee60c2cd67fb2867880cd192d
Maxwell Baird <bairdm71@gmail.com>
2020-02-25 10:03:40 -0700

    added head to head

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -5,7 +5,28 @@ class TeamStats < Stats
     super(games, teams, game_teams)
   end
 
+  def check(hash, key, game)
+    if hash[key] == nil
+      hash[key] = [0,0]
+    end
+    if key >= game
+      hash[key][1] += 1
+    else
+      hash[key][0] += 1
+      hash[key][1] += 1
+    end
+  end
+
   def head_to_head(team_id)
     all_games = games.find_all{ |game| team_id == game.away_team_id || team_id == game.home_team_id}
+    a_hash = {}
+    all_games.each do |game|
+      if team_id == game.away_team_id
+        check(a_hash, game.home_team_id, game.away_team_id)
+      else
+        check(a_hash, game.away_team_id, game.home_team_id)
+      end
+    end
+    a_hash
   end
 end
