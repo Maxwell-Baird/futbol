5d0a831c335439c0a078ffb526c4ebbe7671bdb8
Alex Latham <alexlthm@gmail.com>
2020-02-24 17:01:06 -0700

    Add method for favorite team

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -4,4 +4,38 @@ class TeamStats < Stats
   def initialize(games, teams, game_teams)
     super(games, teams, game_teams)
   end
+
+  def favorite_opponent(team_id)
+    # find team that loses to this team most often
+    games_with_team = @games.select do |game|
+      game.away_team_id == team_id || game.home_team_id == team_id
+    end
+
+    win_ratios = Hash.new { |hash, key| hash[key] = [0,0] }
+
+    games_with_team.each do |game|
+      if game.away_goals > game.home_goals
+        win_ratios[game.away_team_id][0] += 1
+      end
+      if game.home_goals > game.away_goals
+        win_ratios[game.home_team_id][0] += 1
+      end
+      win_ratios[game.away_team_id][1] += 1
+      win_ratios[game.home_team_id][1] += 1
+    end
+
+    # look into hash merge instead of each with object
+    win_percentages = win_ratios.each_with_object(Hash.new) do |(team_id, win_ratio), win_percent|
+      win_percent[team_id] = win_ratio[0].fdiv(win_ratio[1]) * 100
+    end
+
+    team_id = win_percentages.key(win_percentages.values.min)
+    find_name(team_id)
+  end
+
+  # def rival(team_id)
+  # end
+  #
+  # def biggest_team_blowout(team_id)
+  # end
 end
