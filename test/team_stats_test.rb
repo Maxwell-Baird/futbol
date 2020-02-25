c947de847b634f4916b041d363ac884700d5a5d8
Alex Latham <alexlthm@gmail.com>
2020-02-24 17:00:28 -0700

    Add test for favorite team

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -15,4 +15,27 @@ class TeamStatsTest < Minitest::Test
     @stat_tracker = StatTracker.from_csv(@locations)
     @team_stats = TeamStats.new(@stat_tracker.games, @stat_tracker.teams, @stat_tracker.game_teams)
   end
+
+  def test_it_can_name_a_favorite_oppponent_team
+    game_path = './data/games_truncated_with_winningest_team.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams_truncated.csv'
+    locations = {
+                games: game_path,
+                teams: team_path,
+                game_teams: game_teams_path
+              }
+    stat_tracker = StatTracker.from_csv(locations)
+    team_stats = TeamStats.new(stat_tracker.games, stat_tracker.teams, stat_tracker.game_teams)
+
+    assert_equal "Seattle Sounders FC", team_stats.favorite_opponent(3)
+  end
+
+  def test_it_can_name_a_rival_team
+    skip
+  end
+
+  def test_it_can_return_biggest_team_blowout
+    skip
+  end
 end
