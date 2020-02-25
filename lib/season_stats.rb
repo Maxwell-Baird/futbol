c6fdd8dda1cb8228c4e06274b170e254383299d4
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-25 10:54:39 -0700

    Remove merge conflict lines

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -5,11 +5,7 @@ class SeasonStats < Stats
   def initialize(games, teams, game_teams)
     super(games, teams, game_teams)
   end
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 007fd85e3c70889f308ab3e6572f9e6050e44f36
   def shot_accuracy_by_team_id(team_id)
     (total_goals_by_team_id(team_id).to_f/total_shots_by_team_id(team_id) * 100.0)
     .round(2)
