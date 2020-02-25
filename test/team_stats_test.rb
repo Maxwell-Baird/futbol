e03a6a4a3b999951a053718bcaef96ef751bafad
Alex Latham <alexlthm@gmail.com>
2020-02-24 17:18:46 -0700

    Fix test for rival (was calling wrong method before in assertion)

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -32,7 +32,18 @@ class TeamStatsTest < Minitest::Test
   end
 
   def test_it_can_name_a_rival_team
-    skip
+    game_path = './data/games_truncated_with_winningest_team.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams_truncated.csv'
+    locations = {
+                games: game_path,
+                teams: team_path,
+                game_teams: game_teams_path
+              }
+    stat_tracker = StatTracker.from_csv(locations)
+    team_stats = TeamStats.new(stat_tracker.games, stat_tracker.teams, stat_tracker.game_teams)
+
+    assert_equal "Atlanta United", team_stats.rival(3)
   end
 
   def test_it_can_return_biggest_team_blowout
