89b22b89179b2ee4df2b5989505e0a4f729a17db
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-25 16:46:24 -0700

    Add test for most/least accurate teams

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -20,15 +20,6 @@ class SeasonStatsTest < Minitest::Test
     assert_instance_of SeasonStats, @season_stats
   end
 
-  def test_it_has_attributes
-    assert_instance_of Array, @season_stats.games
-    assert_instance_of Game, @season_stats.games.first
-    assert_instance_of Array, @season_stats.teams
-    assert_instance_of Team, @season_stats.teams.first
-    assert_instance_of Array, @season_stats.game_teams
-    assert_instance_of GameTeam, @season_stats.game_teams.first
-  end
-
   def test_it_returns_name_of_team_with_most_tackles
     assert_equal "Houston Dynamo", @season_stats.most_tackles("20132014")
   end
@@ -51,5 +42,11 @@ class SeasonStatsTest < Minitest::Test
 
   def test_worst_coach
     assert_equal "John Tortorella", @season_stats.worst_coach("20122013")
+  end
+
+  def test_it_can_return_hash_with_shots_and_goals_per_team
+    expected = {24=>0.375, 20=>0.5, 26=>0.16666666666666666}
+
+    assert_equal expected, @season_stats.shots_and_goals_per_team("20142015")  
   end
 end
