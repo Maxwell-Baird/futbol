84fd030995ff03436d89a52e9af24a7182afba34
Alex Latham <alexlthm@gmail.com>
2020-02-26 18:09:07 -0700

    Add attributes test to SeasonStats

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -27,6 +27,15 @@ class SeasonStatsTest < Minitest::Test
     assert_instance_of SeasonStats, @season_stats
   end
 
+  def test_it_has_attributes
+    assert_instance_of Array, @season_stats.games
+    assert_instance_of Game, @season_stats.games.first
+    assert_instance_of Array, @season_stats.teams
+    assert_instance_of Team, @season_stats.teams.first
+    assert_instance_of Array, @season_stats.game_teams
+    assert_instance_of GameTeam, @season_stats.game_teams.first
+  end
+
   def test_it_returns_name_of_team_with_most_tackles
     assert_equal "Houston Dynamo", @season_stats.most_tackles("20132014")
   end
